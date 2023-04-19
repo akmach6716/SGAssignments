574d7f41f3ae4b866ac1cfe538c5e9b1878d8cac
akmach <akmach6716@gmail.com>
2023-04-19 13:20:53 +0530

    running successfully now

--- DownloadFile.java
+++ DownloadFile.java
@@ -10,17 +10,23 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class DownloadFile {
 	public static void main(String[] args) {
 		//WebDriver driver;
-		File downloadFolder = new File("E://Sample path");
-		downloadFolder.mkdir();
+		String downloadPath=System.getProperty("user.dir");
 		ChromeOptions options = new ChromeOptions();		
 		HashMap<String,Object> chromeprefs=new HashMap<String, Object>();
-		chromeprefs.put("download.default_directory", downloadFolder);
+		chromeprefs.put("profile.default_content_settings.popups", 0);
+		chromeprefs.put("download.default_directory", downloadPath);
 		options.setExperimentalOption("prefs", chromeprefs);
 		//DesiredCapabilities cap=DesiredCapabilities.Chrome();
 		WebDriver driver=new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/download");
 		driver.findElement(By.xpath("//a[contains(text(),'LambdaTest')]")).click();
+		File f=new File(downloadPath+"/LambdaTest.txt");
+
+		if(f.exists())
+		{
+			System.out.println("file found ");
+		}else {System.out.println("file not found in the path ");}
 	}
 
 }
