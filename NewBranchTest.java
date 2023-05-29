873f38566d10f730978a426ed4eebcc331400e0a
akmach <akmach6716@gmail.com>
2023-05-29 12:44:18 +0530

    New branch added

---
+++ NewBranchTest.java
@@ -0,0 +1,12 @@
+package sgPractice.Assignments;
+
+public class NewBranchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+public void createsomething()
+{
+	}
+}
---
+++ Testgit.java
@@ -0,0 +1,16 @@
+package sgPractice.Assignments;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+
+
+	}
+	public void test1(){
+		
+	}
+	
+public void test2(){
+		
+	}
+}
